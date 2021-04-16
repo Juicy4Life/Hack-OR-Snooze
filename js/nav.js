4d9f97b77ca30d5eaaa02bbd6abe32f160cc6ec2
Elvia <etcamarena@gmail.com>
2021-04-16 12:16:32 -0700

    added HTML form for story submission and navigation

--- js/nav.js
+++ js/nav.js
@@ -34,3 +34,12 @@ function updateNavOnLogin() {
   $navLogOut.show();
   $navUserProfile.text(`${currentUser.username}`).show();
 }
+
+$body.on("click", "#nav-submit", navSubmitStoryClick);
+
+function navSubmitStoryClick(evt) {
+  console.debug("navSubmitStoryClick", evt);
+  hidePageComponents();
+  $submitForm.show();
+
+}
